9b6abd3d073e53fd399a0cd80f5bb17bb6b1ed5b
Mekentosh <m87022928044@gmail.com>
2023-08-07 02:39:04 +0600

    4

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,2 +1,32 @@
-public class WeightedGraph {
+import java.util.*;
+
+public class WeightedGraph<V extends Comparable<V>> {
+    private Map<Vertex<V>, List<Vertex<V>>> map;
+
+    public WeightedGraph() {
+        map = new HashMap<>();
+    }
+
+    public void addVertex(Vertex<V> vertex) {
+        map.put(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(Vertex<V> source, Vertex<V> destination, double weight) {
+        if (!map.containsKey(source) || !map.containsKey(destination)) {
+            throw new IllegalArgumentException("Vertex is not found in the graph");
+        }
+        source.addAdjacentVertex(destination, weight);
+        map.get(source).add(destination);
+    }
+
+    public List<Vertex<V>> getAdjacentVertices(Vertex<V> vertex) {
+        if (!map.containsKey(vertex)) {
+            throw new IllegalArgumentException("Vertex " + vertex + " is out of range!");
+        }
+        return map.get(vertex);
+    }
+
+    public Set<Vertex<V>> getAllVertices() {
+        return map.keySet();
+    }
 }
