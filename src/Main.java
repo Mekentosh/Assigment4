384bb5302aca286fe25e57be61146aaad23d5a8f
Mekentosh <m87022928044@gmail.com>
2023-08-07 02:39:35 +0600

    4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        WeightedGraph<Integer> graph = new WeightedGraph<>();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Vertex<Integer> vertex1 = new Vertex<>(1);
+        Vertex<Integer> vertex2 = new Vertex<>(2);
+        Vertex<Integer> vertex3 = new Vertex<>(3);
+        Vertex<Integer> vertex4 = new Vertex<>(4);
+        Vertex<Integer> vertex5 = new Vertex<>(5);
+        Vertex<Integer> vertex6 = new Vertex<>(6);
+        Vertex<Integer> vertex7 = new Vertex<>(7);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        graph.addVertex(vertex1);
+        graph.addVertex(vertex2);
+        graph.addVertex(vertex3);
+        graph.addVertex(vertex4);
+        graph.addVertex(vertex5);
+        graph.addVertex(vertex6);
+        graph.addVertex(vertex7);
+
+        graph.addEdge(vertex1, vertex2, 3);
+        graph.addEdge(vertex1, vertex3, 6);
+        graph.addEdge(vertex2, vertex4, 2);
+        graph.addEdge(vertex2, vertex5, 4);
+        graph.addEdge(vertex3, vertex6, 1);
+        graph.addEdge(vertex3, vertex7, 4);
+
+        Search<Integer> bfsSearch = new BreadthFirstSearch<>();
+        List<Vertex<Integer>> bfsPath = bfsSearch.search(graph, vertex1, vertex7);
+        System.out.println("BFS Path: " + bfsPath);
+
+        Search<Integer> dijkstraSearch = new DijkstraSearch<>();
+        List<Vertex<Integer>> dijkstraPath = dijkstraSearch.search(graph, vertex1, vertex7);
+        System.out.println("Dijkstra Path: " + dijkstraPath);
     }
 }
